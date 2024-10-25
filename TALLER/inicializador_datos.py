afcd3e47f7d638787b77381163005fa769d8e9d9
jesuslop96 <jesuslopez1296@gmail.com>
2024-10-24 21:01:58 -0500

    inicializador_datos.py

--- TALLER/inicializador_datos.py
+++ TALLER/inicializador_datos.py
@@ -1,29 +1,18 @@
-import os
 import json
+import os
+
 
 def inicializar_archivo_json():
-    Ruta_Datos = os.path.join(os.path.dirname(__file__), 'datos', 'Inventory.json')
-    os.makedirs(os.path.dirname(Ruta_Datos), exist_ok=True)
+    ruta_datos = os.path.join(os.path.dirname(__file__), 'datos', 'Inventory.json')
+    os.makedirs(os.path.dirname(ruta_datos), exist_ok=True)
 
-    if not os.path.exists(Ruta_Datos):
-        Productos_creados = [
-            {
-                "Producto": "Arroz",
-                "Cantidad": 100,
-                "Precio": 2000
-            },
-            {
-                "Producto": "Frijoles",
-                "Cantidad": 5,
-                "Precio": 2000
-            }
-        ]
-        with open(Ruta_Datos, 'w') as archivo:
-            json.dump(Productos_creados, archivo, indent=2)
-            print(f"Archivo {Ruta_Datos} creado e inicializado con Productos de ejemplo.")
+    if not os.path.exists(ruta_datos):
+        with open(ruta_datos, 'w') as archivo:
+            json.dump(cuentas_iniciales, archivo, indent=2)
+            print(f"Archivo {ruta_datos} creado e inicializado con cuentas de ejemplo.")
     else:
-        print(f"El Archivo {Ruta_Datos} ya existe.")
+        print(f"El archivo {ruta_datos} ya existe.")
+
 
 if __name__ == "__main__":
     inicializar_archivo_json()
-
